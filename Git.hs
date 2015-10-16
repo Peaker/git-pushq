f26790fb400849a05d18df26f8b9cebd6214194e
Eyal Lotem <eyal.lotem@gmail.com>
2015-10-16 22:10:04 +0300

    Initial

---
+++ Git.hs
@@ -0,0 +1,130 @@
+{-# OPTIONS -Wall #-}
+module Git
+    ( GitCommandFailed(..)
+    , fetch
+    , BranchLocation(..)
+    , RepoPath
+    , Remote
+    , Branch
+    , CommitID
+    , RefSpec
+    , listBranches
+    , commitsBetween
+    , branchCheckout
+    , branchNewCheckout
+    , branchDelete
+    , branchDeleteRemote
+    , branchRename
+    , commit
+    , rebase
+    , revParse
+    ) where
+
+import Control.Exception (Exception, throwIO)
+import Control.Monad (void)
+import Data.List (isSuffixOf)
+import Data.Typeable (Typeable)
+import System.Exit (ExitCode(..))
+import System.Process
+
+type Remote = String
+type RepoPath = FilePath
+type Branch = String
+type RefSpec = String
+type CommitID = String
+
+data GitCommandFailed = GitCommandFailed
+    { gitCommand :: String
+    , gitCommandArgs :: [String]
+    , gitCommandExitCode :: Int
+    , gitCommandStdout :: String
+    , gitCommandStderr :: String
+    } deriving (Typeable)
+instance Show GitCommandFailed where
+    show (GitCommandFailed cmd args errCode out err) =
+        concat
+        [ "git ", cmd, " ", show args, " failed with error code:"
+        , show errCode, ".\n\n"
+        , if null out then "" else "git output was:\n" ++ out
+        , if null err then "" else "git stderr was:\n" ++ err
+        ]
+instance Exception GitCommandFailed
+
+git :: RepoPath -> String -> [String] -> IO String
+git repoPath cmd args =
+    do  (exitCode, out, err) <- readCreateProcessWithExitCode process stdin
+        case exitCode of
+            ExitSuccess -> return out
+            ExitFailure i -> throwIO (GitCommandFailed cmd args i out err)
+    where
+        stdin = ""
+        process = CreateProcess
+            { cmdspec = RawCommand "git" (cmd:args)
+            , cwd = Just repoPath
+            , env = Nothing
+            , std_in = CreatePipe
+            , std_out = CreatePipe
+            , std_err = Inherit
+            , close_fds = True
+            , create_group = False
+            , delegate_ctlc = False
+            }
+
+fetch :: RepoPath -> Remote -> IO ()
+fetch repoPath remoteName =
+    do  _ <- git repoPath "remote" ["prune", remoteName]
+        _ <- git repoPath "fetch" [remoteName]
+        return ()
+
+data BranchLocation = LocalBranch | RemoteBranch | AllBranches
+
+listBranches :: RepoPath -> BranchLocation -> IO [Branch]
+listBranches repoPath loc =
+    map (drop 2) . lines <$> git repoPath "branch" opts
+    where
+        opts = case loc of
+            LocalBranch -> []
+            RemoteBranch -> ["-r"]
+            AllBranches -> ["-a"]
+
+-- | equivalent to git log src..dest (exclusive src/inclusive dest)
+-- only hashes
+commitsBetween :: RepoPath -> RefSpec -> RefSpec -> IO [CommitID]
+commitsBetween repoPath src dest =
+    lines <$> git repoPath "log" ["--pretty=format:%h", src ++ ".." ++ dest]
+
+branchCheckout :: RepoPath -> Branch -> IO ()
+branchCheckout repoPath name =
+    void $ git repoPath "checkout" [name, "--"]
+
+branchNewCheckout :: RepoPath -> Branch -> RefSpec -> IO ()
+branchNewCheckout repoPath name pos =
+    void $ git repoPath "checkout" ["-b", name, pos, "--"]
+
+branchDelete :: RepoPath -> Branch -> IO ()
+branchDelete repoPath name =
+    void $ git repoPath "branch" ["-D", name]
+
+branchDeleteRemote :: RepoPath -> Remote -> Branch -> IO ()
+branchDeleteRemote repoPath remoteName name =
+    void $ git repoPath "push" [remoteName, "--delete", name]
+
+branchRename :: RepoPath -> Branch -> Branch -> IO ()
+branchRename repoPath old new =
+    void $ git repoPath "branch" ["-M", old, new]
+
+stripNewline :: String -> String
+stripNewline xs
+    | "\n" `isSuffixOf` xs = init xs
+    | otherwise = xs
+
+commit :: RepoPath -> String -> IO ()
+commit repoPath msg =
+    void $ git repoPath "commit" ["-m", msg, "--allow-empty"]
+
+rebase :: RepoPath -> RefSpec -> IO ()
+rebase repoPath refSpec =
+    void $ git repoPath "rebase" [refSpec]
+
+revParse :: RepoPath -> RefSpec -> IO CommitID
+revParse repoPath refSpec = stripNewline <$> git repoPath "rev-parse" [refSpec]
---
+++ PushQ.hs
@@ -0,0 +1,153 @@
+{-# LANGUAGE TupleSections #-}
+{-# OPTIONS -Wall #-}
+import           Control.Concurrent
+import           Control.Exception (catch, throwIO, SomeException(..))
+import           Control.Monad
+import           Data.List (stripPrefix, sort)
+import           Data.Maybe (fromMaybe, mapMaybe)
+import           Data.Monoid ((<>))
+import           Data.Time.Clock (NominalDiffTime)
+import           GHC.Stack (whoCreated)
+import qualified Git
+import           Opts (parseOpts, Opts(..))
+
+-- TODO: configurable remote name
+remoteName :: Git.Remote
+remoteName = "origin"
+
+filterBranchesByPrefix ::
+    Git.RepoPath -> String -> Git.BranchLocation -> IO [(String, Git.Branch)]
+filterBranchesByPrefix repoPath prefix branchPos =
+    do  refs <- Git.listBranches repoPath branchPos
+        return (mapMaybe pair refs)
+    where
+        pair name = (, name) <$> stripPrefix prefix name
+
+testingBranchSplitName :: String -> Maybe (Int, String)
+testingBranchSplitName str =
+    case break (=='/') str of
+    (_, "") -> Nothing
+    (numStr, '/':name) ->
+        case reads numStr of
+        [(num, "")] -> Just (num, name)
+        _ -> Nothing
+    _ -> error "break returns non-empty snd that doesn't match the break predicate?"
+
+getTestingBranches :: Git.RepoPath -> IO [(Int, String, Git.Branch)]
+getTestingBranches repoPath =
+    do  branches <-
+            map extractPrefix
+            <$> filterBranchesByPrefix repoPath "testing/" Git.LocalBranch
+        return $ sort branches
+    where
+        extractPrefix (fullName, branch) =
+            (num, name, branch)
+            where
+                (num, name) = fromMaybe err $ testingBranchSplitName fullName
+        err = error $ "/testing/* branch format should be <num>-<name>"
+
+infixl 1 `onGitFail`
+onGitFail :: IO a -> IO a -> IO a
+act `onGitFail` handler = act `catch` \Git.GitCommandFailed {} -> handler
+
+ignoreGitError :: IO () -> IO ()
+ignoreGitError = (`onGitFail` return ())
+
+reject :: Git.RepoPath -> String -> Git.RefSpec -> String -> IO ()
+reject repoPath name refSpec msg =
+    do  putStrLn $ "rejecting " ++ name ++ ":\n" ++ msg
+        newRejectBranch `catch` \failure ->
+            putStrLn $ "Failed to reject branch, git output:\n" ++ Git.gitCommandStderr failure
+    where
+        newRejectBranch =
+            do  Git.branchNewCheckout repoPath ("rejected/" ++ name) refSpec
+                Git.commit repoPath msg
+
+-- | Rebase an orig/<name> branch on top of the given point into testing/<name>
+-- Returns the new result commit (tip of rebased)
+attemptRebase :: Git.RepoPath -> (String, Git.Branch) -> String -> IO (Maybe Git.CommitID)
+attemptRebase repoPath (name, testingBranch) base =
+    do  Git.branchCheckout repoPath base
+        ignoreGitError (Git.branchDelete repoPath testingBranch)
+        Git.branchNewCheckout repoPath testingBranch origBranch
+        (Just testingBranch <$ Git.rebase repoPath base)
+            `onGitFail`
+            do  Git.branchCheckout repoPath origBranch
+                Git.branchDelete repoPath testingBranch
+                baseCommitId <- Git.revParse repoPath base
+                reject repoPath name "HEAD" $ concat
+                    [ "Rejected due to rebase failure (on "
+                    , base, "[", baseCommitId, "])" ]
+                Git.branchCheckout repoPath base
+                Git.branchDelete repoPath origBranch
+                return Nothing
+    where
+        origBranch = "orig/" ++ name
+
+-- | Rebase all testing branches to be fast-forwards of one another
+rebaseTestingBranches :: Git.RepoPath -> IO Git.Branch
+rebaseTestingBranches repoPath =
+    do  branches <- getTestingBranches repoPath
+        (rebased, attempted, res) <- loop 0 0 "master" branches
+        putStrLn $ concat
+            [ show (length branches), " testing branches exists (successfully rebased "
+            , show rebased, " of ", show attempted, ")"]
+        return res
+    where
+        loop :: Int -> Int -> Git.RefSpec -> [(Int, String, Git.RefSpec)] -> IO (Int, Int, Git.RefSpec)
+        loop rebased attempted prev [] = return (rebased, attempted, prev)
+        loop rebased attempted prev ((_num, name, branch):nexts) =
+            do  divergence <- Git.commitsBetween repoPath branch prev
+                (onRebased, onAttempted, newPos) <-
+                    if null divergence
+                    then return (id, id, branch)
+                    else maybe (id, (+1), prev) ((,,) (+1) (+1))
+                         <$> attemptRebase repoPath (name, branch) prev
+                loop (onRebased rebased) (onAttempted attempted) newPos nexts
+
+poll :: Git.RepoPath -> IO ()
+poll repoPath =
+    do  Git.fetch repoPath remoteName
+        proposed <-
+            filterBranchesByPrefix repoPath (remoteName <> "/q/") Git.RemoteBranch
+        base <- rebaseTestingBranches repoPath
+        let nextId = maybe 1 fst $ testingBranchSplitName base
+        unless (null proposed) $
+            do  putStrLn $ "Rebasing proposed branches on top of " ++ show base ++ ":"
+                mapM_ (putStrLn . fst) proposed
+        loop nextId base proposed
+    where
+        loop _ _ [] = return ()
+        loop nextId prev ((name, branch):nexts) =
+            do  q <- Git.revParse repoPath branch
+                Git.branchDeleteRemote repoPath remoteName ("q/" ++ name)
+                success <-
+                    True <$ Git.branchNewCheckout repoPath ("orig/" ++ name) q
+                    `onGitFail`
+                    False <$
+                    ( reject repoPath name q $ concat
+                      ["Rejected, test of ", show name, " already in progress!"]
+                    )
+                when success $
+                    do  let newTestingBranch = "testing/" ++ show nextId ++ "/" ++ name
+                        newBase <-
+                            fromMaybe prev
+                            <$> attemptRebase repoPath (name, newTestingBranch) prev
+                        loop (nextId+1) newBase nexts
+
+sleep :: NominalDiffTime -> IO ()
+sleep = threadDelay . truncate . (1000000 *)
+
+run :: Opts -> IO ()
+run (Opts interval repoPath) =
+    forever $
+    do  poll repoPath
+        sleep interval
+
+main :: IO ()
+main =
+    do  opts <- parseOpts
+        run opts
+    `catch` \e@SomeException {} ->
+        do  mapM_ putStrLn =<< whoCreated e
+            throwIO e
